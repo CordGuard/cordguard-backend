4840a7824f0ee97d6be59c1fc9a146c6d75ec8a0
DEvil Al-Ghamdi <github@v0id.me>
2024-11-19 10:12:08 +0300

    chore: increasing the max tokens and adding the basemodel for the request class

--- routes/ai_api.py
+++ routes/ai_api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException, Request
+from pydantic import BaseModel
 import logging
 from cordguard_database import CordGuardAnalysisStatus, CordGuardDatabase
 from cordguard_ai import OpenAIMaliciousTextDetector
@@ -8,9 +9,9 @@ import os
 logging.basicConfig(level=logging.INFO)
 
 ai_api_endpoint_router = APIRouter(prefix="/ai/api", tags=["ai"])
-HARDCODED_MAX_TOKENS = 2048
+HARDCODED_MAX_TOKENS = 64800
 
-class AIDetectRequest:
+class AIDetectRequest(BaseModel):
     text: str
     signed_hwid: str
     analysis_id: str
@@ -23,13 +24,16 @@ async def detect(request: AIDetectRequest, full_request: Request = None):
     db: CordGuardDatabase = await CordGuardDatabase.create()
     logging.info("Database instance created.")
 
-    if not is_sub_host(full_request, os.getenv('AI_API_HOST', 'ai.')):
-        logging.warning("Unauthorized access attempt from host: %s", full_request.client.host)
-        raise HTTPException(status_code=403, detail="AI API only allowed through AI subdomain")
-    
-    if full_request.headers.get('x-api-key') != os.getenv('AI_API_KEY'):
-        logging.warning("Invalid API key provided.")
-        raise HTTPException(status_code=403, detail="Invalid AI API key")
+    if os.getenv('DEBUG') == 'true':
+        logging.info('DEBUG is true, skipping host check')
+    else:
+        if not is_sub_host(full_request, os.getenv('AI_API_HOST', 'ai.')):
+            logging.warning("Unauthorized access attempt from host: %s", full_request.client.host)
+            raise HTTPException(status_code=403, detail="AI API only allowed through AI subdomain")
+        
+        if full_request.headers.get('x-api-key') != os.getenv('AI_API_KEY'):
+            logging.warning("Invalid API key provided.")
+            raise HTTPException(status_code=403, detail="Invalid AI API key")
 
     # Is it already in the database?
     ai_response = await db.get_ai_response_by_analysis_id(request.analysis_id)
@@ -42,11 +46,11 @@ async def detect(request: AIDetectRequest, full_request: Request = None):
     logging.info("Estimated tokens for the request: %d", estimated_tokens)
     
     if estimated_tokens > HARDCODED_MAX_TOKENS:
-        logging.error("Text is too long - exceeds 2048 tokens")
-        raise HTTPException(status_code=400, detail="Text is too long - exceeds 2048 tokens")
+        logging.error(f"Text is too long - exceeds {HARDCODED_MAX_TOKENS} tokens")
+        raise HTTPException(status_code=400, detail=f"Text is too long - exceeds {HARDCODED_MAX_TOKENS} tokens")
     
     # Get machine from signed_hwid
-    machine = await db.get_machine_by_signed_hwid(request.signed_hwid)
+    machine = await db.get_worker_by_signed_hwid(request.signed_hwid)
     if machine is None:
         logging.error("Machine not found for signed_hwid: %s", request.signed_hwid)
         raise HTTPException(status_code=404, detail="Machine not found")
@@ -57,12 +61,12 @@ async def detect(request: AIDetectRequest, full_request: Request = None):
         logging.error("Analysis record not found for analysis_id: %s", request.analysis_id)
         raise HTTPException(status_code=404, detail="Analysis record not found")
     
-    # Check if the analysis record is pending
-    if analysis_record.status != CordGuardAnalysisStatus.PENDING:
-        logging.error("Analysis is not pending for analysis_id: %s", request.analysis_id)
-        raise HTTPException(status_code=400, detail="Analysis is not pending")
+    # Check if the analysis record is analyzing
+    if analysis_record.status != CordGuardAnalysisStatus.ANALYZING:
+        logging.error("Analysis is not analyzing for analysis_id: %s", request.analysis_id)
+        raise HTTPException(status_code=400, detail="Analysis is not analyzing")
     
-    detector = OpenAIMaliciousTextDetector()
+    detector = OpenAIMaliciousTextDetector(auto_model=True)
     response = detector.detect(request.text)
     await db.save_ai_response(request.analysis_id, request.text, response.get_dict())
     logging.info("AI response saved for analysis_id: %s", request.analysis_id)
